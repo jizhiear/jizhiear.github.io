bcde75a9a3060ae4fb12d551782b1addcae38ffe
jizhiear <126840973+jizhiear@users.noreply.github.com>
2023-06-10 22:20:44 +0800

    This is jizhiear's website.

---
+++ jizhiear‘s website/script.js
@@ -0,0 +1,10 @@
+function sayHello(){
+  var response = prompt("What is your name?");
+  alert("Hello," + response +",have a nice day!");
+}
+
+function add(a,b){
+return a + b;
+}
+console.log(add(20,30));
+console.log(add(200,300));
